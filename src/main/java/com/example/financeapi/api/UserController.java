67c076314b37c66bb939e1be02d07b97c2f0d0d8
Kyjko <bognarmiklos34@gmail.com>
2021-04-21 12:32:56 +0200

    frontend-demo

--- src/main/java/com/example/financeapi/api/UserController.java
+++ src/main/java/com/example/financeapi/api/UserController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin
-@RequestMapping("/api/v1/users")
 @RestController
 public class UserController {
     private final UserService userService;
@@ -20,49 +19,49 @@ public class UserController {
         this.userService = userService;
     }
 
+    @RequestMapping("/api/v1/users/get")
     @GetMapping(path = "{id}")
     public User getUserById(@PathVariable("id") UUID id) {
         return userService.getUserById(id);
     }
 
-    @GetMapping(path = "{name}")
-    public User getUserByName(@PathVariable("name") String name) {
-        return userService.getUserByName(name);
-    }
-
+    @RequestMapping("/api/v1/users/delete")
     @DeleteMapping(path = "{id}")
     public int deleteUserById(@PathVariable("id") UUID id) {
         return userService.deleteUserById(id);
     }
 
+    @RequestMapping("/api/v1/users")
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
+    @RequestMapping("/api/v1/users/update")
     @PutMapping(path = "{id}")
     public int updateUserById(@PathVariable("id") UUID id, @RequestBody final User user) {
         return userService.updateUserById(id, user);
     }
 
-    @RequestMapping("/api/v1/users/transactions")
+    @RequestMapping("/api/v1/users/transactions/get")
     @GetMapping(path = "{id}")
     public List<Transaction> getTransactionHistoryOfUser(@PathVariable("id") UUID id) {
         return userService.getTransactionHistoryOfUser(id);
     }
 
-    @RequestMapping("api/v1/transactions")
+    @RequestMapping("/api/v1/transactions")
     @GetMapping
     public List<Transaction> getTransactionHistory() {
         return userService.getTransactionHistory();
     }
 
-    @RequestMapping("api/v1/transactions")
+    @RequestMapping("/api/v1/transactions/push")
     @PostMapping
     public int pushTransaction(@RequestBody final Transaction tr) {
         return userService.pushTransaction(tr);
     }
 
+    @RequestMapping("/api/v1/users/add")
     @PostMapping
     public int addUser(@RequestBody final User user) {
         return userService.addUser(user);
