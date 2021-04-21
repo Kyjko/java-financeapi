fa7a34d36cb6ee1169a9dda6aa94f4b475952cac
Kyjko <bognarmiklos34@gmail.com>
2021-04-21 08:49:34 +0200

    updated transactions

--- src/main/java/com/example/financeapi/model/User.java
+++ src/main/java/com/example/financeapi/model/User.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
 import java.util.UUID;
 
 @Entity
